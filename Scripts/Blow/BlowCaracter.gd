1950d0477188ed03fa52b961865d0efda3c3fd1b
hrmvlg <hrmvlg@mail.ru>
2021-06-05 15:30:17 +0300

    Changes in: Blow level; Menu buttons.

--- Scripts/Blow/BlowCaracter.gd
+++ Scripts/Blow/BlowCaracter.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 onready var animation = $AnimationPlayer
-var is_animation_done = false
 
 func _ready():
 	animation.play("entry")
@@ -14,22 +13,27 @@ func character_blowing():
 	
 func exercise_completed():
 	animation.play("exercise_completed")
-
+	
 func character_talking():
 	animation.play("talking")
 
+func stopAnim():
+	animation.stop()
+
 func end_of_anim():
-	if(get_parent().get_node("Trigger_scene").successful_attempts < 3):
+	var successful_attempts = get_parent().get_node("Trigger_scene").successful_attempts
+
+	if(successful_attempts < 3):
 		get_parent().get_node("Trigger_scene").state = "on"
 		get_parent().get_node("Trigger_scene").interaction_time.start()
 #		включаем таймер на ожидание выполения упражнения
 		get_parent().get_node("Trigger_scene").waiting_time.start()
-		print("я сработала")
-	elif (get_parent().get_node("Trigger_scene").successful_attempts == 3):
+		print(successful_attempts)
+
+	elif (successful_attempts == 3):
 		print("задание выполнено успешно 3 раза")
+		get_parent().get_node("Trigger_scene").LevelCompleted()
 		exercise_completed()
-		#задержка 2 секунды перед закрытием уровня после 3 выполнений задания
-		yield(get_tree().create_timer(2.0), "timeout")
-		get_tree().change_scene("res://Scenes/Map/Map_scene.tscn")
-	else:
-		pass
+#		задержка 2 секунды перед закрытием уровня после 3 выполнений задания
+#		yield(get_tree().create_timer(2.0), "timeout")
+#		get_tree().change_scene("res://Scenes/Map/Map_scene.tscn")
--- Scripts/Blow/BlowTrigger.gd
+++ Scripts/Blow/BlowTrigger.gd
@@ -9,8 +9,11 @@ var noize_status
 var level_progress
 var character
 var successful_attempts = 0
+var unsuccesful_attempts = 0
 var waiting_time
 var waiting_label
+var nextlvlv_button
+var refresh_button
 
 func _ready():
 	interaction_time = get_node("TimeOfInteraction")
@@ -21,6 +24,8 @@ func _ready():
 	character = get_parent().get_node("Main_character")
 	waiting_time = get_node("Waiting_time")
 	waiting_label = get_parent().get_node("Label")
+	nextlvlv_button = get_parent().get_node("NextLvl_button")
+	refresh_button = get_parent().get_node("Refresh_button")
 	
 func _process(delta):
 	power = stepify(AudioServer.\
@@ -28,21 +33,19 @@ func _process(delta):
 	get_bus_index("Blow_bus"), 0), 0.01)
 	if (state == "on"):
 		waiting_label.text = stepify(waiting_time.time_left, 0) as String
-#		переделать в сигнал timeout ???
 		if(waiting_time.time_left == 0):
 			EndOfWaitingTime()
-		if (power > -30): 
+		if (power > -40): 
 			interaction_time_label.text = stepify(interaction_time.time_left, 0.001) as String
 			character.character_blowing()
 			waiting_time.start()
-		if (power < -30):
+		if (power < -40):
 			BlowTrigger()
 			interaction_time.start()
 		mic_volume.text = power as String
 	
 func BlowTrigger():
 	if(interaction_time.time_left < 4):
-		noize_status.text = "Noize"
 		level_progress.text = "Passed"
 		character.exercise_completed() 	
 		character.character_talking()
@@ -50,11 +53,36 @@ func BlowTrigger():
 			successful_attempts+=1
 			state = "off"
 	else:
-		noize_status.text = "Not Noize"
+#		это выполняется буквально все время после начала 
 		level_progress.text = "Not Passed"
 		
 func EndOfWaitingTime():
-	state = "off"
-	print("сообщение о том что время истекло")
+	unsuccesful_attempts+=1
+	if(unsuccesful_attempts<4):
+		state = "off"
+		waiting_label.text = "Ну-же попробуй!"
+		Restart()
+	else:
+		nextlvlv_button.visible = true
+		waiting_label.text = "Ну-же попробуй!"
+		Restart()
+
+func Restart():
 	character.character_talking()
 	waiting_time.start()
+	interaction_time.start()
+
+func _on_TimeOfInteraction_timeout():
+	successful_attempts+=1
+	if(successful_attempts< 3):
+		state = "off"
+		interaction_time.stop()
+		Restart()
+
+func LevelCompleted():
+	state = "off"
+	waiting_label.text = "Всё!"
+	refresh_button.visible = true
+	character.animation.stop()
+	print("Уровень пройден кнопка пройти заново доступна")
+	
